27efc62f729b915bcd82c40a9a76e5cbd07f8641
thinkgo <thinkgo@aliyun.com>
2025-07-10 10:57:10 +0800

    fix: fix webmid

---
+++ webmid/metadata.go
@@ -0,0 +1,18 @@
+package webmid
+
+import "github.com/gin-gonic/gin"
+
+const ExclusivelyTitleKey = "/gin/exclusively/title"
+
+// Title set the title value in `*gin.Context`.
+func Title(title string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set(ExclusivelyTitleKey, title)
+		c.Next()
+	}
+}
+
+// GetTitle returns the title value stored in `*gin.Context`, if any.
+func GetTitle(c *gin.Context) string {
+	return c.GetString(ExclusivelyTitleKey)
+}
--- webmid/skip_filter.go
+++ webmid/skip_filter.go
@@ -1,4 +1,4 @@
-package webutil
+package webmid
 
 import (
 	"slices"
